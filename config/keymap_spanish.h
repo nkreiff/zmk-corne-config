4d0aaf782bd78ee674dc3db43ab1b67edd57a16d
Nicolás Kreiff <nicolas.kreiff@mercadolibre.com>
2024-10-02 20:57:02 -0300

    test: us_egne

---
+++ config/keymap_spanish.h
@@ -0,0 +1,84 @@
+#pragma once
+#include "keycodes.h"
+// clang-format off
+
+// Aliases
+#define ES_MORD KC_GRV  // º
+#define ES_1    KC_1    // 1
+#define ES_2    KC_2    // 2
+#define ES_3    KC_3    // 3
+#define ES_4    KC_4    // 4
+#define ES_5    KC_5    // 5
+#define ES_6    KC_6    // 6
+#define ES_7    KC_7    // 7
+#define ES_8    KC_8    // 8
+#define ES_9    KC_9    // 9
+#define ES_0    KC_0    // 0
+#define ES_QUOT KC_MINS // '
+#define ES_IEXL KC_EQL  // ¡
+#define ES_Q    KC_Q    // Q
+#define ES_W    KC_W    // W
+#define ES_E    KC_E    // E
+#define ES_R    KC_R    // R
+#define ES_T    KC_T    // T
+#define ES_Y    KC_Y    // Y
+#define ES_U    KC_U    // U
+#define ES_I    KC_I    // I
+#define ES_O    KC_O    // O
+#define ES_P    KC_P    // P
+#define ES_GRV  KC_LBRC // ` (dead)
+#define ES_PLUS KC_RBRC // +
+#define ES_A    KC_A    // A
+#define ES_S    KC_S    // S
+#define ES_D    KC_D    // D
+#define ES_F    KC_F    // F
+#define ES_G    KC_G    // G
+#define ES_H    KC_H    // H
+#define ES_J    KC_J    // J
+#define ES_K    KC_K    // K
+#define ES_L    KC_L    // L
+#define ES_NTIL KC_SCLN // Ñ
+#define ES_ACUT KC_QUOT // ´ (dead)
+#define ES_CCED KC_NUHS // Ç
+#define ES_LABK KC_NUBS // <
+#define ES_Z    KC_Z    // Z
+#define ES_X    KC_X    // X
+#define ES_C    KC_C    // C
+#define ES_V    KC_V    // V
+#define ES_B    KC_B    // B
+#define ES_N    KC_N    // N
+#define ES_M    KC_M    // M
+#define ES_COMM KC_COMM // ,
+#define ES_DOT  KC_DOT  // .
+#define ES_MINS KC_SLSH // -
+#define ES_FORD LS(ES_MORD) // ª
+#define ES_EXLM LS(ES_1)    // !
+#define ES_DQUO LS(ES_2)    // "
+#define ES_BULT LS(ES_3)    // ·
+#define ES_DLR  LS(ES_4)    // $
+#define ES_PERC LS(ES_5)    // %
+#define ES_AMPR LS(ES_6)    // &
+#define ES_SLSH LS(ES_7)    // /
+#define ES_LPRN LS(ES_8)    // (
+#define ES_RPRN LS(ES_9)    // )
+#define ES_EQL  LS(ES_0)    // =
+#define ES_QUES LS(ES_QUOT) // ?
+#define ES_IQUE LS(ES_IEXL) // ¿
+#define ES_CIRC LS(ES_GRV)  // ^ (dead)
+#define ES_ASTR LS(ES_PLUS) // *
+#define ES_DIAE LS(ES_ACUT) // ¨ (dead)
+#define ES_RABK LS(ES_LABK) // >
+#define ES_SCLN LS(KC_COMM) // ;
+#define ES_COLN LS(KC_DOT)  // :
+#define ES_UNDS LS(ES_MINS) // _
+#define ES_BSLS RA(ES_MORD) // (backslash)
+#define ES_PIPE RA(ES_1)    // |
+#define ES_AT   RA(ES_2)    // @
+#define ES_HASH RA(ES_3)    // #
+#define ES_TILD RA(ES_4)    // ~
+#define ES_EURO RA(ES_5)    // €
+#define ES_NOT  RA(ES_6)    // ¬
+#define ES_LBRC RA(ES_GRV)  // [
+#define ES_RBRC RA(ES_PLUS) // ]
+#define ES_LCBR RA(ES_ACUT) // {
+#define ES_RCBR RA(ES_CCED) // }
